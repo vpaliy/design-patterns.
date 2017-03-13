f975057c16821c86f6295b9cfa3b6f1b499e049c
Vasya Paliy <vpaliy97@gmail.com>
2017-03-12 22:14:10 -0400

    Observer::WeatherExample completed

---
+++ Observer/src/test/java/weatherExample/data/DataTest.java
@@ -0,0 +1,11 @@
+package weatherExample.data;
+
+import WeatherExample.data.WeatherDataProvider;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class DataTest {
+
+}
